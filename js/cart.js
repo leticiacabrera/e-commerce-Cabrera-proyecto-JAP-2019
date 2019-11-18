3f4e8a9951587cc7dbbb455e87474ecf649b0ad5
leticiacabrera <55068359+leticiacabrera@users.noreply.github.com>
2019-11-18 00:50:11 -0300

    Obligatorio 4ta entrega

--- js/cart.js
+++ js/cart.js
@@ -1,28 +1,21 @@
 //const CART_INFO_URL = "https://api.myjson.com/bins/aj865";
 //const CART_BUY_URL = "https://api.myjson.com/bins/10ils5";
 
-
 var product = {};
 let canti = 0;
-
 let productUnitCost = 0;
-/*
-let productCurrency = "";*/
 let subtotal = 0;
 let shippingPercentage = 0.15; // Porcentaje de envío 
 let total = 0;
 let totalCostToShow = 0;
-/*
-let paymentTypeSelected = false;
-const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
-const BANKING_PAYMENT = "Transferencia bancaria";*/
 let ERROR_MSG = "Ha habido un error :(, verifica qué pasó.";
-
-
+const CREDIT_CARD_PAYMENT = "Tarjeta de crédito";
+const BANKING_PAYMENT = "Transferencia bancaria";
+var selecciono = false;
 
 //Función que se utiliza para actualizar los costos de publicación
 
-function totCostPerArticle() { ///*function updateSubtotal(){
+function totCostPerArticle() {
     productUnitCost = article.unitCost;
     canti = parseInt(document.getElementById("productCountInput").value);
 
@@ -33,32 +26,14 @@ function totCostPerArticle() { ///*function updateSubtotal(){
 }
 
 
-
 function updateTotalCosts(){
-    /*let totalCostHTML = document.getElementById("totalCostText").innerHTML;*/
-
     let comissionToShow = Math.round(shippingPercentage * 100) + "%";
     let totCostToShow = Math.round(totalCostToShow + ((shippingPercentage * 100) * totalCostToShow) / 100);
-    /*let totalCostToShow = (Math.round(productCost * shippingPercentage * 100) / 100);*/
 
     document.getElementById("comissionText").innerHTML = comissionToShow;
     document.getElementById("totalCostText").innerHTML = totCostToShow;
 }
 
-//hacer un arreglo de subtotales adentro del for
-
-
-/*Mostrar tipo de pago no seleccionado*/
-/*
-function showPaymentTypeNotSelected(){
-
-}
-
-function hidePaymentTypeNotSelected(){
-
-}
-<td>` + article.unitCost + `&nbsp` + article.currency + `</td>
-*/
 
 function showArticles(array){
 
@@ -96,7 +71,6 @@ function showArticles(array){
 }
 
 
-
 //Función que se ejecuta una vez que se haya lanzado el evento de
 //que el documento se encuentra cargado, es decir, se encuentran todos los
 //elementos HTML presentes.
@@ -105,9 +79,6 @@ document.addEventListener("DOMContentLoaded", function(e){
         if (resultObj.status === 'ok') {
             product = resultObj.data;
             showArticles(product.articles);
-            
-            /*document.getElementById('productCostText').innerHTML = updateSubtotal();
-            document.getElementById('totalCostText').innerHTML = updateTotalCosts();*/
         }
     });
     
@@ -142,6 +113,7 @@ document.addEventListener("DOMContentLoaded", function(e){
         let cornerInput = document.getElementById("corner");
         let countrySelect = document.getElementById("country");
         let infoMissing = false;
+
 
         //Quito las clases que marcan como inválidos
         streetInput.classList.remove('is-invalid');
@@ -178,7 +150,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             countrySelect.classList.add('is-invalid');
             infoMissing = true;
         }
-        
+
         if(!infoMissing)
         {
             //Aquí ingresa si pasó los controles, irá a enviar
@@ -206,70 +178,20 @@ document.addEventListener("DOMContentLoaded", function(e){
         if (e.preventDefault) e.preventDefault();
             return false;
     }); 
-      
-    //Se obtiene el formulario de forma de pago
-    var payForm = document.getElementById("pay-info");
-
-    //Se agrega una escucha en el evento 'submit' que será
-    //lanzado por el formulario cuando se seleccione 'Terminar'.
-    payForm.addEventListener("submit", function(e){
-
-        
-        let cardNumberInput = document.getElementById("creditCardNumber");
-        let cardSecurityInput = document.getElementById("creditCardSecurityCode");
-        let dateInput = document.getElementById("dueDate");
-        let bankInput = document.getElementById("bankAccountNumber");
-        let infoMissing = false;
-
-        //Quito las clases que marcan como inválidos
-        cardNumberInput.classList.remove('is-invalid');
-        cardSecurityInput.classList.remove('is-invalid');
-        dateInput.classList.remove('is-invalid');
-        bankInput.classList.remove('is-invalid');
-
-        //Se realizan los controles necesarios,
-        //En este caso se controla que se haya ingresado la dirección.
-        //Consulto por el nombre del producto
-
-        //Consulto por el costo
-        if (cardNumberInput.value === "")
-        {
-            cardNumberInput.classList.add('is-invalid');
-            infoMissing = true;
-        }
-
-        //Consulto por el costo
-        if (cardSecurityInput.value === "")
-        {
-            cardSecurityInput.classList.add('is-invalid');
-            infoMissing = true;
-        }
-
-        if (dateInput.value === "")
-        {
-            dateInput.classList.add('is-invalid');
-            infoMissing = true;
-        }
-
-        if (bankInput.value === "")
-        {
-            bankInput.classList.add('is-invalid');
-            infoMissing = true;
-        }
-        
-        if(!infoMissing)
-        {
-            //Aquí ingresa si pasó los controles, irá a enviar
-            //la solicitud para crear la publicación.
-            
-        }
-
-        //Esto se debe realizar para prevenir que el formulario se envíe (comportamiento por defecto del navegador)
-        if (e.preventDefault) e.preventDefault();
-            return false;
-
-    
-    }); 
-
- 
+});
+document.getElementById("creditCardPaymentRadio").addEventListener("change", function(){
+        document.getElementById("creditCardNumber").disabled=false;
+        document.getElementById("creditCardSecurityCode").disabled=false;
+        document.getElementById("dueDate").disabled=false;
+        document.getElementById("bankAccountNumber").disabled=true;
+        document.getElementById("span1").innerHTML = CREDIT_CARD_PAYMENT
+        selecciono = true;
+});
+document.getElementById("bankingRadio").addEventListener("change", function(){
+        document.getElementById("creditCardNumber").disabled=true;
+        document.getElementById("creditCardSecurityCode").disabled=true;
+        document.getElementById("dueDate").disabled=true;
+        document.getElementById("bankAccountNumber").disabled=false;
+        document.getElementById("span1").innerHTML = BANKING_PAYMENT
+        selecciono = true;
 });
